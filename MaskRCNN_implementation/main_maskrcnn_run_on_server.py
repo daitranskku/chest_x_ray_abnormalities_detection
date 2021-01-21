af5ed2e95c38ad64ddab1e95ed829deeb633d0eb
daitranskku <daitran@skku.edu>
2021-01-21 17:05:34 +0900

    Update train on server

---
+++ MaskRCNN_implementation/main_maskrcnn_run_on_server.py
@@ -0,0 +1,212 @@
+# Check GPU
+import tensorflow as tf
+gpu = len(tf.config.list_physical_devices('GPU'))>0
+print("GPU is", "available" if gpu else "NOT AVAILABLE")
+
+from tensorflow.python.client import device_lib
+print("GPU sample processing: ")
+print(device_lib.list_local_devices())
+
+# Import libraries
+import os
+import json
+import random
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import cv2
+import itertools
+from tqdm import tqdm
+import pydicom
+from pydicom.pixel_data_handlers.util import apply_voi_lut
+
+import warnings
+warnings.filterwarnings("ignore")
+
+# Data dir
+DATA_DIR = "/home/dairesearch/data/kaggle/data/"
+TRAIN_CSV_DIR = os.path.join(DATA_DIR, "org_train.csv")
+
+PREPROCESSED_TRAINING_IMAGE_FOLDER = '/home/dairesearch/data/kaggle/data/512_jpg/'
+
+# Read original csv
+orin_df = pd.read_csv(TRAIN_CSV_DIR)
+orin_df = orin_df.query('class_id != 14')
+
+# Helper functions
+
+def get_mask(img_dimensions, x_min, y_min, x_max, y_max):
+    img_height, img_width = img_dimensions
+    img_mask = np.full((img_height,img_width),0)
+    img_mask[y_min:y_max,x_min:x_max] = 255
+    return img_mask.astype(np.float32)
+def rle_encoding(x):
+    dots = np.where(x.T.flatten() == 255)[0]
+    run_lengths = []
+    prev = -2
+    for b in dots:
+        if (b>prev+1): run_lengths.extend((b + 1, 0))
+        run_lengths[-1] += 1
+        prev = b
+    return ' '.join([str(x) for x in run_lengths])
+
+def dicom2array(path, voi_lut=True, fix_monochrome=True):
+    dicom = pydicom.read_file(path)
+    # VOI LUT (if available by DICOM device) is used to
+    # transform raw DICOM data to "human-friendly" view
+    if voi_lut:
+        data = apply_voi_lut(dicom.pixel_array, dicom)
+    else:
+        data = dicom.pixel_array
+    # depending on this value, X-ray may look inverted - fix that:
+    if fix_monochrome and dicom.PhotometricInterpretation == "MONOCHROME1":
+        data = np.amax(data) - data
+
+    data = data - np.min(data)
+    data = data / np.max(data)
+    data = (data * 255).astype(np.uint8)
+
+    return data
+
+
+# Load training df
+training_df = pd.read_csv('/home/dairesearch/data/kaggle/data/512_jpg_df.csv', converters ={'EncodedPixels': eval, 'CategoryId': eval})
+samples_df = training_df
+print(samples_df)
+
+
+# Import mrcnn
+
+from mrcnn.config import Config
+from mrcnn import utils
+import mrcnn.model as modellib
+from mrcnn import visualize
+from mrcnn.model import log
+
+NUM_CATS = 14
+IMAGE_SIZE = 512
+
+class DiagnosticConfig(Config):
+    NAME = "Diagnostic"
+    NUM_CLASSES = NUM_CATS + 1 # +1 for the background class
+
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 5 #That is the maximum with the memory available on kernels
+
+    BACKBONE = 'resnet50'
+
+    IMAGE_MIN_DIM = IMAGE_SIZE
+    IMAGE_MAX_DIM = IMAGE_SIZE
+    IMAGE_RESIZE_MODE = 'none'
+
+    POST_NMS_ROIS_TRAINING = 250
+    POST_NMS_ROIS_INFERENCE = 150
+    MAX_GROUNDTRUTH_INSTANCES = 5
+    BACKBONE_STRIDES = [4, 8, 16, 32, 64]
+    BACKBONESHAPE = (8, 16, 24, 32, 48)
+    RPN_ANCHOR_SCALES = (8,16,24,32,48)
+    ROI_POSITIVE_RATIO = 0.33
+    DETECTION_MAX_INSTANCES = 300
+    DETECTION_MIN_CONFIDENCE = 0.7
+    # STEPS_PER_EPOCH should be the number of instances
+    # divided by (GPU_COUNT*IMAGES_PER_GPU), and so should VALIDATION_STEPS;
+    # however, due to the time limit, I set them so that this kernel can be run in 9 hours
+    STEPS_PER_EPOCH = int(len(samples_df)*0.9/IMAGES_PER_GPU)
+    VALIDATION_STEPS = len(samples_df)-int(len(samples_df)*0.9/IMAGES_PER_GPU)
+
+config = DiagnosticConfig()
+config.display()
+
+category_list = orin_df.class_name.unique()
+
+
+
+class DiagnosticDataset(utils.Dataset):
+    def __init__(self, df):
+        super().__init__(self)
+
+        # Add classes
+        for i, name in enumerate(category_list):
+            self.add_class("diagnostic", i+1, name)
+
+        # Add images
+        for i, row in df.iterrows():
+            self.add_image("diagnostic",
+                           image_id=row.name,
+                           path= PREPROCESSED_TRAINING_IMAGE_FOLDER+str(row.image_id)+".jpg",
+                           labels=row['CategoryId'],
+                           annotations=row['EncodedPixels'],
+                           height=row['Height'], width=row['Width'])
+
+    def image_reference(self, image_id):
+        info = self.image_info[image_id]
+        return info['path'], [category_list[int(x)] for x in info['labels']]
+
+    def load_image(self, image_id):
+
+        return cv2.imread(self.image_info[image_id]['path'])
+
+    def load_mask(self, image_id):
+        info = self.image_info[image_id]
+
+        mask = np.zeros((IMAGE_SIZE, IMAGE_SIZE, len(info['annotations'])), dtype=np.uint8)
+        labels = []
+        for m, (annotation, label) in enumerate(zip(info['annotations'], info['labels'])):
+            sub_mask = np.full(info['height']*info['width'], 0, dtype=np.uint8)
+
+            annotation = [int(x) for x in annotation.split(' ')]
+
+            for i, start_pixel in enumerate(annotation[::2]):
+                sub_mask[start_pixel: start_pixel+annotation[2*i+1]] = 1
+
+            sub_mask = sub_mask.reshape((info['height'], info['width']), order='F')
+            sub_mask = cv2.resize(sub_mask, (IMAGE_SIZE, IMAGE_SIZE), interpolation=cv2.INTER_NEAREST)
+
+            mask[:, :, m] = sub_mask
+            labels.append(int(label)+1)
+        return mask, np.array(labels)
+
+# Train/ Val split
+training_percentage = 0.9
+
+training_set_size = int(training_percentage*len(samples_df))
+validation_set_size = int((1-training_percentage)*len(samples_df))
+
+train_dataset = DiagnosticDataset(samples_df[:training_set_size])
+train_dataset.prepare()
+
+valid_dataset = DiagnosticDataset(samples_df[training_set_size:training_set_size+validation_set_size])
+valid_dataset.prepare()
+
+# Load weight
+WEIGHT_PATH = '/home/dairesearch/home/dairesearch/chest_x_ray_abnormalities_detection/MaskRCNN_implementation/weights/mask_rcnn_coco.h5'
+
+# Create model and load pretrained weights
+
+LR = 1e-4
+EPOCHS = 10
+
+model = modellib.MaskRCNN(mode='training', config=config, model_dir="")
+
+model.load_weights(WEIGHT_PATH, by_name=True, exclude=['mrcnn_class_logits', 'mrcnn_bbox_fc', 'mrcnn_bbox', 'mrcnn_mask'])
+
+history = model.train(train_dataset, valid_dataset,
+            learning_rate=LR,
+            epochs=EPOCHS,
+            layers='heads')
+
+
+
+
+
+
+
+
+
+
+
+
+
